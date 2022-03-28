5a9bfbd3a9e2bccabfa38637fa4ffff312ba3983
angelicaalbuquerque <angelica.o.albuquerque@gmail.com>
2022-03-28 17:17:59 -0300

    Tic-tac-toe done

--- src/components/Square/index.tsx
+++ src/components/Square/index.tsx
@@ -1,15 +1,57 @@
+import { useState, Dispatch, SetStateAction, useEffect } from "react";
 import "./Square.css";
 
-type squareProps = {
-  children?: React.ReactNode;
-  dataCol: number;
-  dataRow: number;
-};
+interface SquareProps {
+  className: string;
+  whosPlaying: string;
+  setWhosPlaying: Dispatch<SetStateAction<string>>;
+  localization: number;
+  setPlayer1: Dispatch<SetStateAction<number[]>>;
+  player1: number[];
+  setPlayer2: Dispatch<SetStateAction<number[]>>;
+  player2: number[];
+  winner: boolean;
+  restart: boolean;
+  setRestart: Dispatch<SetStateAction<boolean>>;
+}
 
-export const Square = ({ children, dataCol, dataRow }: squareProps) => {
+const Square = ({
+  className,
+  whosPlaying,
+  setWhosPlaying,
+  localization,
+  setPlayer1,
+  player1,
+  setPlayer2,
+  player2,
+  winner,
+  restart,
+  setRestart,
+}: SquareProps) => {
+  const [XorCircle, setXorCircle] = useState("");
+
+  useEffect(() => {
+    setXorCircle("");
+    setRestart(false);
+  }, [restart]);
+
+  const play = () => {
+    if (XorCircle === "" && whosPlaying === "X" && winner === false) {
+      setXorCircle("X");
+      setWhosPlaying("O");
+      setPlayer1([...player1, localization]);
+    } else if (XorCircle === "" && whosPlaying === "O" && winner === false) {
+      setXorCircle("O");
+      setWhosPlaying("X");
+      setPlayer2([...player2, localization]);
+    }
+  };
+
   return (
-    <span className="square" data-col={dataCol} data-row={dataRow}>
-      {children}
-    </span>
+    <div className={className} onClick={play}>
+      {XorCircle}
+    </div>
   );
 };
+
+export default Square;
--- src/pages/App/index.tsx
+++ src/pages/App/index.tsx
@@ -1,19 +1,210 @@
-import { Board } from "../../components/Board";
-import { Button } from "../../components/Button";
-import { GameState } from "../../components/GameState";
 import "./App.css";
+import Square from "../../components/Square";
+import { Fragment, useEffect, useState } from "react";
 
 function App() {
-  const { gamePlay, checkWin, announceWinner } = GameState();
+  const [player1, setPlayer1] = useState<number[]>([]);
+  const [player2, setPlayer2] = useState<number[]>([]);
+  const [whosPlaying, setWhosPlaying] = useState("X");
+
+  const [winner, setWinner] = useState(false);
+
+  const [restart, setRestart] = useState(false);
+
+  useEffect(() => {
+    if (player1.includes(1) && player1.includes(2) && player1.includes(3)) {
+      return setWinner(true), setWhosPlaying("X");
+    }
+    if (player2.includes(1) && player2.includes(2) && player2.includes(3)) {
+      return setWinner(true), setWhosPlaying("O");
+    }
+
+    if (player1.includes(4) && player1.includes(5) && player1.includes(6)) {
+      return setWinner(true), setWhosPlaying("X");
+    }
+    if (player2.includes(4) && player2.includes(5) && player2.includes(6)) {
+      return setWinner(true), setWhosPlaying("O");
+    }
+    if (player1.includes(7) && player1.includes(8) && player1.includes(9)) {
+      return setWinner(true), setWhosPlaying("X");
+    }
+    if (player2.includes(7) && player2.includes(8) && player2.includes(9)) {
+      return setWinner(true), setWhosPlaying("O");
+    }
+    if (player1.includes(1) && player1.includes(4) && player1.includes(7)) {
+      return setWinner(true), setWhosPlaying("X");
+    }
+    if (player2.includes(1) && player2.includes(4) && player2.includes(7)) {
+      return setWinner(true), setWhosPlaying("O");
+    }
+    if (player1.includes(2) && player1.includes(5) && player1.includes(8)) {
+      return setWinner(true), setWhosPlaying("X");
+    }
+    if (player2.includes(2) && player2.includes(5) && player2.includes(8)) {
+      return setWinner(true), setWhosPlaying("O");
+    }
+    if (player1.includes(3) && player1.includes(6) && player1.includes(9)) {
+      return setWinner(true), setWhosPlaying("X");
+    }
+    if (player2.includes(3) && player2.includes(6) && player2.includes(9)) {
+      return setWinner(true), setWhosPlaying("O");
+    }
+    if (player1.includes(1) && player1.includes(5) && player1.includes(9)) {
+      return setWinner(true), setWhosPlaying("X");
+    }
+    if (player2.includes(1) && player2.includes(5) && player2.includes(9)) {
+      return setWinner(true), setWhosPlaying("O");
+    }
+    if (player1.includes(3) && player1.includes(5) && player1.includes(7)) {
+      return setWinner(true), setWhosPlaying("X");
+    }
+    if (player2.includes(3) && player2.includes(5) && player2.includes(7)) {
+      return setWinner(true), setWhosPlaying("O");
+    }
+  }, [player1, player2]);
+
+  const playAgain = () => {
+    setWinner(false);
+    setPlayer1([]);
+    setPlayer2([]);
+    setRestart(true);
+  };
 
   return (
-    <div className="App">
+    <Fragment>
       <h1 className="Title">Tic-Tac-Toe</h1>
-      <Board />
-      <div className="BtnContainer">
-        <Button onClick={() => {}}>Play again!</Button>
+      {winner && <h2 className="Subtitle">Player "{whosPlaying}" Winner!</h2>}
+
+      <div className="FirstLine">
+        <Square
+          className="firstSquare"
+          whosPlaying={whosPlaying}
+          setWhosPlaying={setWhosPlaying}
+          localization={1}
+          player1={player1}
+          setPlayer1={setPlayer1}
+          player2={player2}
+          setPlayer2={setPlayer2}
+          winner={winner}
+          restart={restart}
+          setRestart={setRestart}
+        />
+        <Square
+          className="secondSquare"
+          whosPlaying={whosPlaying}
+          setWhosPlaying={setWhosPlaying}
+          localization={2}
+          player1={player1}
+          setPlayer1={setPlayer1}
+          player2={player2}
+          setPlayer2={setPlayer2}
+          winner={winner}
+          restart={restart}
+          setRestart={setRestart}
+        />
+        <Square
+          className="thirdSquare"
+          whosPlaying={whosPlaying}
+          setWhosPlaying={setWhosPlaying}
+          localization={3}
+          player1={player1}
+          setPlayer1={setPlayer1}
+          player2={player2}
+          setPlayer2={setPlayer2}
+          winner={winner}
+          restart={restart}
+          setRestart={setRestart}
+        />
       </div>
-    </div>
+
+      <div className="SecondLine">
+        <Square
+          className="fourthSquare"
+          whosPlaying={whosPlaying}
+          setWhosPlaying={setWhosPlaying}
+          localization={4}
+          player1={player1}
+          setPlayer1={setPlayer1}
+          player2={player2}
+          setPlayer2={setPlayer2}
+          winner={winner}
+          restart={restart}
+          setRestart={setRestart}
+        />
+        <Square
+          className="fifthSquare"
+          whosPlaying={whosPlaying}
+          setWhosPlaying={setWhosPlaying}
+          localization={5}
+          player1={player1}
+          setPlayer1={setPlayer1}
+          player2={player2}
+          setPlayer2={setPlayer2}
+          winner={winner}
+          restart={restart}
+          setRestart={setRestart}
+        />
+        <Square
+          className="sixthSquare"
+          whosPlaying={whosPlaying}
+          setWhosPlaying={setWhosPlaying}
+          localization={6}
+          player1={player1}
+          setPlayer1={setPlayer1}
+          player2={player2}
+          setPlayer2={setPlayer2}
+          winner={winner}
+          restart={restart}
+          setRestart={setRestart}
+        />
+      </div>
+
+      <div className="ThirdLine">
+        <Square
+          className="seventhSquare"
+          whosPlaying={whosPlaying}
+          setWhosPlaying={setWhosPlaying}
+          localization={7}
+          player1={player1}
+          setPlayer1={setPlayer1}
+          player2={player2}
+          setPlayer2={setPlayer2}
+          winner={winner}
+          restart={restart}
+          setRestart={setRestart}
+        />
+        <Square
+          className="eighthSquare"
+          whosPlaying={whosPlaying}
+          setWhosPlaying={setWhosPlaying}
+          localization={8}
+          player1={player1}
+          setPlayer1={setPlayer1}
+          player2={player2}
+          setPlayer2={setPlayer2}
+          winner={winner}
+          restart={restart}
+          setRestart={setRestart}
+        />
+        <Square
+          className="ninthSquare"
+          whosPlaying={whosPlaying}
+          setWhosPlaying={setWhosPlaying}
+          localization={9}
+          player1={player1}
+          setPlayer1={setPlayer1}
+          player2={player2}
+          setPlayer2={setPlayer2}
+          winner={winner}
+          restart={restart}
+          setRestart={setRestart}
+        />
+      </div>
+
+      <div className="BtnContainer">
+        <button onClick={playAgain}>Play again!</button>
+      </div>
+    </Fragment>
   );
 }
 
